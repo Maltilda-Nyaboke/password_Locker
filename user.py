8fe22e6a4cc792f461b3f7c6c1ce7aeb36f9ffb5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-23 19:34:22 +0300

    style:create new class testuser

--- user.py
+++ user.py
@@ -1,7 +1,15 @@
 class User:
-    
- user_list=[]
+   '''
+     Class user which generates new instances in the user accounts
+   ''' 
+   user_list=[]
  
-def __init__(self,username,password):
+   def __init__(self,username,password):
+    '''
+    use of the __init__method that helps in construction of an object
+    Args:
+        username = new user username.
+        password = new user password.
+    '''
     self.username = username
     self.password = password
