e050a6f31e991cdd74c3f4a9772b2fe1db32890f
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-24 00:58:08 +0300

    test:initialize new user

--- user_test.py
+++ user_test.py
@@ -2,3 +2,14 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
+    '''
+    class Test that defines test cases for user class behaviours.
+
+    Args:
+        unittest.Testcase:Testcase class that helps in creating test cases
+    '''
+def setUp(self):
+    '''
+    set up method to run before each test case
+    ''' 
+    self.new_user= User("Tilda","Welcome!")   
