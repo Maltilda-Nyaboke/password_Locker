b61bb60f8993527a3244fb465a1ac31b85f31416
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-24 01:53:21 +0300

    style:compare object initialization

--- user_test.py
+++ user_test.py
@@ -2,14 +2,30 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
+    
     '''
-    class Test that defines test cases for user class behaviours.
+    Test class that defines test cases for user class behaviours.
 
     Args:
         unittest.Testcase:Testcase class that helps in creating test cases
     '''
-def setUp(self):
-    '''
-    set up method to run before each test case
-    ''' 
-    self.new_user= User("Tilda","Welcome!")   
+
+    def setUp(self):
+   
+        '''
+        set up method to run before each test case
+        ''' 
+        
+        self.new_user= User("Tilda","Welcome!")   
+
+    def test_init(self):
+        '''
+        test_init test case to see if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,"Tilda")
+        self.assertEqual(self.new_user.password,"Welcome!")
+
+    def test_save_user(self):
+        
+if __name__ == '__main__':
+    unittest.main()
