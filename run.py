9eea81faa7f026ba97a5ba2acef000a29e5132d5
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-24 22:54:18 +0300

    style:retriving credentials

--- run.py
+++ run.py
@@ -23,7 +23,36 @@ def del_user(user):
     '''
     user.delete_user()
 
-def get_user(user)    
+def create_credentials(site_name,username, password):
+    '''
+    Create  new user"s credentials
+    '''
+    new_credentials = Credentials(site_name,username, password)
+    return new_credentials
+
+def save_credentials(credentials):
+    '''
+    function to save the user credentials
+    '''
+    credentials.save_credentials()
+
+def del_credentials(credentials):
+    '''
+    function to delete site credentials
+    '''
+    credentials.delete_user()
+
+def display_credentials(credentials):
+    '''
+    function to display credentials
+    ''' 
+    return Credentials.display_credentials()   
+
+def get_credentials(name):
+    '''
+    function to retrieve credentials
+    '''  
+    return Credentials.get_credentials(name)  
 
 
     if __name__ == '__main__':
