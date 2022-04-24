f492163c11c0adb4e64043fa896167f021ea70f9
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-04-24 04:13:17 +0300

    feature:add save user method

--- user.py
+++ user.py
@@ -1,15 +1,23 @@
+
 class User:
    '''
-     Class user which generates new instances in the user accounts
-   ''' 
-   user_list=[]
+   Class user which generates new instances in the user accounts
+   '''
+   user_list = []
  
    def __init__(self,username,password):
-    '''
-    use of the __init__method that helps in construction of an object
-    Args:
-        username = new user username.
-        password = new user password.
-    '''
-    self.username = username
-    self.password = password
+        '''
+        use of the __init__method that helps in construction of an object
+        Args:
+            username = new user username.
+            password = new user password.
+        '''
+        self.username = username
+        self.password = password
+
+   def save_user(self):  
+        '''
+        save_user which is the method used in saving the user contents in the user_list
+        
+        '''
+        User.user_list.append(self)  
--- user_test.py
+++ user_test.py
@@ -6,8 +6,8 @@ class TestUser(unittest.TestCase):
     '''
     Test class that defines test cases for user class behaviours.
 
-    Args:
-        unittest.Testcase:Testcase class that helps in creating test cases
+     Args:
+    unittest.Testcase:Testcase class that helps in creating test cases
     '''
 
     def setUp(self):
@@ -15,7 +15,6 @@ class TestUser(unittest.TestCase):
         '''
         set up method to run before each test case
         ''' 
-        
         self.new_user= User("Tilda","Welcome!")   
 
     def test_init(self):
@@ -26,6 +25,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password,"Welcome!")
 
     def test_save_user(self):
-        
+        '''
+        test used to see if user can be saved in the empty user_list
+        '''
+        self.new_user.save_user() #saving a new user
+        self.assertEqual(len(User.user_list),1)
+
+
 if __name__ == '__main__':
     unittest.main()
